310fb7e698e0da69ac4590aed412f4cb9a3d200d
yunbok <yunjigo92@gmail.com>
2022-09-27 13:38:22 +0900

    Create AdminController Mock
    - admin controller 임의 생성

---
+++ src/main/java/com/yunji/covidgetinline/controller/AdminController.java
@@ -0,0 +1,37 @@
+package com.yunji.covidgetinline.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * description
+ * <p>
+ * author         : yunji
+ * date           : 22. 9. 27.
+ */
+@RequestMapping("/admin")
+@Controller
+public class AdminController {
+
+    @GetMapping("/places")
+    public String adminPlaces(){
+        return "admin/places";
+    }
+
+    @GetMapping("/places/{placeId}")
+    public String adminPlaceDetail(@PathVariable Integer placeId){
+        return "admin/place-detail";
+    }
+
+    @GetMapping("/events")
+    public String adminEvents(){
+        return "admin/events";
+    }
+
+    @GetMapping("/events/{eventId}")
+    public String adminEventDetail(@PathVariable Integer eventId){
+        return "admin/event-detail";
+    }
+}
