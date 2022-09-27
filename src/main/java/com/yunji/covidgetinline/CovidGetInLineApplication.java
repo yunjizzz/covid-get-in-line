c657020a7c67574482c044634f7e87605023484e
yunbok <yunjigo92@gmail.com>
2022-09-27 13:39:39 +0900

    Initial Spring boot
    - spring boot 프로젝트 생성

---
+++ src/main/java/com/yunji/covidgetinline/CovidGetInLineApplication.java
@@ -0,0 +1,13 @@
+package com.yunji.covidgetinline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidGetInLineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidGetInLineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yunji/covidgetinline/CovidGetInLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunji.covidgetinline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidGetInLineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
