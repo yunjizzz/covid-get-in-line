d3952c10bc62097e05d5f19cf91ef41c8efb6654
yunbok <yunjigo92@gmail.com>
2022-10-13 10:05:25 +0900

    Add service logic test
    - 서비스 로직 테스트 추가

---
+++ src/test/java/com/yunji/covidgetinline/controller/error/APIExceptionHandlerTest.java
@@ -0,0 +1,89 @@
+package com.yunji.covidgetinline.controller.error;
+
+import com.yunji.covidgetinline.constant.ErrorCode;
+import com.yunji.covidgetinline.dto.APIErrorResponse;
+import com.yunji.covidgetinline.exception.GeneralException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.handler.DispatcherServletWebRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
+
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * description
+ * <p>
+ * author         : yunji
+ * date           : 22. 10. 11.
+ */class APIExceptionHandlerTest {
+
+    private APIExceptionHandler sut;
+    private WebRequest webRequest;
+
+    @BeforeEach
+    void setUp() {
+        sut = new APIExceptionHandler();
+        webRequest = new DispatcherServletWebRequest(new MockHttpServletRequest());
+    }
+
+    @DisplayName("검증 오류 - 응답 데이터 정의")
+    @Test
+    void givenValidationException_whenCallingValidation_thenReturnsResponseEntity() {
+        // Given
+        ConstraintViolationException e = new ConstraintViolationException(Set.of());
+
+        // When
+        ResponseEntity<Object> response = sut.validation(e, webRequest);
+
+        // Then
+        assertThat(response)
+                .hasFieldOrPropertyWithValue("body", APIErrorResponse.of(false, ErrorCode.VALIDATION_ERROR, e))
+                .hasFieldOrPropertyWithValue("headers", HttpHeaders.EMPTY)
+                .hasFieldOrPropertyWithValue("statusCode", HttpStatus.BAD_REQUEST);
+    }
+
+    @DisplayName("프로젝트 일반 오류 - 응답 데이터 정의")
+    @Test
+    void givenGeneralException_whenCallingValidation_thenReturnsResponseEntity() {
+        // Given
+        ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
+        GeneralException e = new GeneralException(errorCode);
+
+        // When
+        ResponseEntity<Object> response = sut.general(e, webRequest);
+
+        // Then
+        assertThat(response)
+                .hasFieldOrPropertyWithValue("body", APIErrorResponse.of(false, errorCode, e))
+                .hasFieldOrPropertyWithValue("headers", HttpHeaders.EMPTY)
+                .hasFieldOrPropertyWithValue("statusCode", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @DisplayName("기타(전체) 오류 - 응답 데이터 정의")
+    @Test
+    void givenOtherException_whenCallingValidation_thenReturnsResponseEntity() {
+        // Given
+        Exception e = new Exception();
+
+        // When
+        ResponseEntity<Object> response = sut.exception(e, webRequest);
+
+        // Then
+        assertThat(response)
+                .hasFieldOrPropertyWithValue("body", APIErrorResponse.of(false, ErrorCode.INTERNAL_ERROR, e))
+                .hasFieldOrPropertyWithValue("headers", HttpHeaders.EMPTY)
+                .hasFieldOrPropertyWithValue("statusCode", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
